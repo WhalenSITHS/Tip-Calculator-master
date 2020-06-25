cdf9d12790f1f929fe2a6a4763f463b9ddbad756
michael whalen <michael.whalen@sitechhs.com>
2020-06-25 12:54:23 -0400

    basic example

---
+++ src/index.js
@@ -0,0 +1,12 @@
+///console only
+
+//declare the bill
+//let bill = 50; let vs const
+//let bill = "50"
+const bill = 50;
+//decide the tip %
+const tipPercentage = 0.2;
+
+//multiply the tip by the bill
+const tipAmount = Math.round(bill * tipPercentage);
+console.log(tipAmount);
